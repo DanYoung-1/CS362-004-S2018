bdb46f886afff63954ba44f9e4ee3582acedd46f
Dan Y <dnspyo@gmail.com>
2018-05-06 21:49:03 -0700

    quiz commit yeeyee

---
+++ projects/youndani/dominion/unittest1.c
@@ -0,0 +1,22 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+//isGameOver test
+
+void assert(int condition);
+
+int main(int argc, char* argv[]) {
+    char name[] = "isGameOver()";
+    struct gameState state;
+    int numPlayers = 2;
+    int seed = 100;
+
+    
+    
+
+
+}
+
